60b750c20de813411b009eef72780ed7c01bb375
yuvankart <148793815+yuvankart@users.noreply.github.com>
2024-03-15 17:40:42 +0530

    Update classifier.py

--- part-4/classifier.py
+++ part-4/classifier.py
@@ -1,9 +1,7 @@
 import csv
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
+# Load data from output.csv
 files = "output.csv"
 outs = []
 c1 = []
@@ -23,31 +21,55 @@ with open(files, 'r') as file:
             cc3 = row[3].strip('[]').split()
             c3.append([int(element) for element in cc3])
         count += 1
-
+print(cc1)
 outs = np.array(outs)
 c1 = np.array(c1)
 c2 = np.array(c2)
 c3 = np.array(c3)
 
-X = np.hstack((c1, c2, c3))
-y = np.array(outs)
+# Combine color channels into feature vectors
+X_train = np.hstack((c1, c2, c3))
+y_train = np.array(outs)
 
-new_input = np.array([[0, 0, 0], [48, 31, 205], [250, 250, 250]])
+# Define KNN classifier
+class KNNClassifier:
+    def __init__(self, k):
+        self.k = k
+    
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def predict(self, X_test):
 
-def cal_dis(inp1, inp2):
-    d_x = np.sqrt((inp1[0][0] - inp2[0])**2 + (inp1[0][1] - inp2[1])**2 + (inp1[0][2] - inp2[2])**2)
-    d_y = np.sqrt((inp1[1][0] - inp2[3])**2 + (inp1[1][1] - inp2[4])**2 + (inp1[1][2] - inp2[5])**2)
-    d_z = np.sqrt((inp1[2][0] - inp2[6])**2 + (inp1[2][1] - inp2[7])**2 + (inp1[2][2] - inp2[8])**2)
-    dis = np.sqrt(d_x**2 + d_y**2 + d_z**2)
-    return dis
-
-distances = [cal_dis(new_input, x) for x in X]
-closest_indices = np.argsort(distances)[:5]
-closest_labels = y[closest_indices]
+        
+        def cal_dis(inp1, inp2):
+                inp1_reshaped = np.reshape(inp1, inp2.shape)
+                return np.linalg.norm(inp1_reshaped - inp2)
 
 
+        distances = [cal_dis(new_input, x) for x in self.X_train]
+        closest_indices = np.argsort(distances)[:5]
+        nearest_labels = self.y_train[closest_indices]
+
+        return np.array(nearest_labels)
+
+# Instantiate and train the KNN classifier
+knn_classifier = KNNClassifier(k=5)
+knn_classifier.fit(X_train, y_train)
+
+# New input data
+new_input = np.array([[0, 0, 0], [48, 31, 20], [250, 250, 250]])
+
+# Reshape new_input correctly
+
+
+# Predict labels for new input using KNN
+knn_predictions = knn_classifier.predict(new_input)
+
+# Save predicted labels to image_names.csv
 with open("image_names.csv", 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerows(map(lambda x: [x], closest_labels))
-    
-print("Predicted labels for the three closest data points:", closest_labels)
+    csv_writer.writerows(map(lambda x: [x], knn_predictions))
+
+print("Predicted labels for the input data points:", knn_predictions)

f527107f1f1344563e5d40dad903d04bfbc326e4
root <root@Dobby>
2024-03-26 05:07:27 +0530

    done

--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+import os
+import csv
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Read image names from the CSV file
+    image_names = []
+    with open('image_names.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            image_names.extend(row)
+
+    # Get the path to the images directory
+    images_dir = os.path.join(app.root_path, 'images')
+
+    return render_template('index.html', image_names=image_names, images_dir=images_dir)
 
 
 if __name__ == '__main__':

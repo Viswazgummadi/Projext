20f1ce23b7b93a8ba50274c0b82dc3c44a588ebb
root <root@Dobby>
2024-03-28 00:57:15 +0530

    done

--- web/app.py
+++ web/app.py
@@ -20,5 +20,18 @@ def index():
     return render_template('index.html', image_names=image_names, images_dir=images_dir)
 
 
+@app.route('/favs.html')
+def favs():
+    image_names = []
+    with open('fav.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            image_names.extend(row)
+
+    # Get the path to the images directory
+    images_dir = os.path.join(app.root_path, 'images')
+
+    return render_template('favs.html', image_names=image_names, images_dir=images_dir)
+
 if __name__ == '__main__':
     app.run(debug=True)

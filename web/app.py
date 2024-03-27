55631e8b37500034919c53e63c0b9f6aa5cd0827
root <root@Dobby>
2024-03-28 01:59:23 +0530

    done

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -32,6 +32,29 @@ def favs():
     images_dir = os.path.join(app.root_path, 'images')
 
     return render_template('favs.html', image_names=image_names, images_dir=images_dir)
+
+
+@app.route('/addToFavorites', methods=['POST'])
+def add_to_favorites():
+    image = request.json['image']
+    # Implement logic to add image to fav.csv
+    with open('fav.csv', 'a') as file:
+        file.write(image + '\n')
+    return 'Image added to favorites'
+
+
+@app.route('/removeFromFavorites', methods=['POST'])
+def remove_from_favorites():
+    image = request.json['image']
+    # Implement logic to remove image from fav.csv
+    with open('fav.csv', 'r') as file:
+        lines = file.readlines()
+    with open('fav.csv', 'w') as file:
+        for line in lines:
+            if line.strip() != image:
+                file.write(line)
+    return 'Image removed from favorites'
+
 
 if __name__ == '__main__':
     app.run(debug=True)

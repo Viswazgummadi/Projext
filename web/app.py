0369cc10ce9adda2ff58fa947977d66ed9a4c811
root <root@Dobby>
2024-03-28 19:47:45 +0530

    fone

--- web/app.py
+++ web/app.py
@@ -1,23 +1,23 @@
 import os
 import csv
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+# Set the secret key
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# Define the upload folder and allowed extensions
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Function to check if a filename has an allowed extension
 
 
 @app.route('/')
 def index():
-    # Read image names from the CSV file
-    image_names = []
-    with open('image_names.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            image_names.extend(row)
-
-    # Get the path to the images directory
-    images_dir = os.path.join(app.root_path, 'images')
-
-    return render_template('index.html', image_names=image_names, images_dir=images_dir)
+    return render_template('index.html')
 
 
 @app.route('/favs.html')
@@ -36,20 +36,16 @@ def favs():
 
 @app.route('/work.html')
 def work():
-    image_names = []
-    with open('fav.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            image_names.extend(row)
-
-    # Get the path to the images directory
-    images_dir = os.path.join(app.root_path, 'images')
-
-    return render_template('work.html', image_names=image_names, images_dir=images_dir)
+    return render_template('work.html')
 
 
 @app.route('/crew.html')
 def crew():
+    return render_template('crew.html')
+
+
+@app.route('/sugg.html')
+def sugg():
     image_names = []
     with open('fav.csv', 'r') as file:
         reader = csv.reader(file)
@@ -59,7 +55,7 @@ def crew():
     # Get the path to the images directory
     images_dir = os.path.join(app.root_path, 'images')
 
-    return render_template('crew.html', image_names=image_names, images_dir=images_dir)
+    return render_template('sugg.html', image_names=image_names, images_dir=images_dir)
 
 
 @app.route('/cart.html')
@@ -76,26 +72,19 @@ def cart():
     return render_template('cart.html', image_names=image_names, images_dir=images_dir)
 
 
-@app.route('/addToFavorites', methods=['POST'])
-def add_to_favorites():
-    image = request.json['image']
-    # Implement logic to add image to fav.csv
-    with open('fav.csv', 'a') as file:
-        file.write(image + '\n')
-    return 'Image added to favorites'
-
-
-@app.route('/removeFromFavorites', methods=['POST'])
-def remove_from_favorites():
-    image = request.json['image']
-    # Implement logic to remove image from fav.csv
-    with open('fav.csv', 'r') as file:
-        lines = file.readlines()
-    with open('fav.csv', 'w') as file:
-        for line in lines:
-            if line.strip() != image:
-                file.write(line)
-    return 'Image removed from favorites'
+# Route to handle image upload and processing
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Process the uploaded file here (you can call your backend code)
+        return redirect(url_for('sugg'))  # Redirect to sugg.html after upload
 
 
 if __name__ == '__main__':
